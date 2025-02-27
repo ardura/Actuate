bdec477d034d91c431ffbbf1d9b04bced49240a7
ardura <azviscarra@gmail.com>
2025-02-27 07:56:00 -0700

    Code cleanup - comments and additive suggested refactors

--- src/audio_module/AdditiveModule.rs
+++ src/audio_module/AdditiveModule.rs
@@ -17,16 +17,7 @@ pub struct AdditiveOscillator {
 impl AdditiveOscillator {
     pub fn default() -> Self {
         AdditiveOscillator {
-            harmonics: {
-                    let mut tmp = Vec::with_capacity(16);
-                    for _ in 0..15 {
-                        tmp.push(AdditiveHarmonic {
-                            index: 0,
-                            amplitude: 0.0,
-                        });
-                    }
-                    tmp
-            },
+            harmonics: vec![AdditiveHarmonic { index: 0, amplitude: 0.0 }; 15],
         }
     }
 
@@ -43,7 +34,7 @@ impl AdditiveOscillator {
             let instant_frequency = util::f32_midi_note_to_freq(base_note).min(nyquist);
             voice.phase_delta = instant_frequency / sample_rate;
 
-            for (i, harmonic) in self.harmonics.iter_mut().enumerate() {
+            for (i, harmonic) in self.harmonics.iter().enumerate() {
                 if harmonic.amplitude != 0.0 {
                     let harmonic_freq = if harmonic.index == 0 {
                         instant_frequency
@@ -85,53 +76,11 @@ impl AdditiveOscillator {
 
         sample
     }
-
-    /*
-    pub fn next_sample(&mut self, voice: &mut SingleVoice, sample_rate: f32, detune_mod: f32, unison_voice: bool) -> f32 {
-        let mut sample = 0.0;
-        let nyquist = sample_rate / 2.0;
-        for (i, harmonic) in self.harmonics.iter_mut().enumerate() {
-            if voice.amp_current != 0.0 && harmonic.amplitude != 0.0 {
-                let base_note: f32;
-                if unison_voice {
-                    base_note = voice.note as f32
-                    + voice._unison_detune_value
-                    + detune_mod
-                    + voice.pitch_current
-                    + voice.pitch_current_2;
-                } else {
-                    base_note = voice.note as f32
-                    + voice._detune
-                    + detune_mod
-                    + voice.pitch_current
-                    + voice.pitch_current_2;
-                }
-                let instant_frequency = util::f32_midi_note_to_freq(base_note).min(nyquist);
-
-                voice.phase_delta = instant_frequency / sample_rate;
-
-                let harmonic_freq = if harmonic.index == 0 {
-                    instant_frequency
-                } else {
-                    (harmonic.index as f32 + 1.0) * instant_frequency
-                };
-                let phase_increment = TAU * harmonic_freq / sample_rate;
-                voice.harmonic_phases[i] = (voice.harmonic_phases[i] + phase_increment) % TAU;
-                sample += voice.harmonic_phases[i].sin() * harmonic.amplitude;
-            }
-        }
-
-        sample
-    }
-    */
 }
 
 // Moon Lander sine approximation w/ changed period and range
 fn fast_sine(mut x: f32) -> f32 {
-    x = x % TAU;
-    if x < 0.0 {
-        x += TAU;
-    }
+    x = x.rem_euclid(TAU);
 
     let sign = if x > PI { -1.0 } else { 1.0 };
     if x > PI {
